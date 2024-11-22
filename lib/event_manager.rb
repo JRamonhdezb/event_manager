2eb9e863165c7c8ebf3b4763282f0db7a37d7f19
JRamonhdezb <141176160+JRamonhdezb@users.noreply.github.com>
2024-11-21 16:02:54 -0800

    Iteration 0: Loading a file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,10 @@
 puts 'Event Manager Initialized'
+
+lines = File.readlines('event_attendees.csv')
+row_index = 0
+lines.each_with_index do |line, index|
+    next if index == 0
+    column = line.split(",")
+    name = column[2]
+    puts name
+end
