ccec203a14ff04621a02003c92f32e982b0a1847
JRamonhdezb <141176160+JRamonhdezb@users.noreply.github.com>
2024-11-21 16:46:51 -0800

    Iteration 2: Cleaning up our zip codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,15 +1,21 @@
 require 'csv'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
 puts 'Event Manager Initialized'
 
 contents = CSV.open(
-    'event_attendees.csv', 
-    headers: true, 
-    header_converters: :symbol
+  'event_attendees.csv', 
+  headers: true, 
+  header_converters: :symbol
 )
 
 contents.each do |row|
-    name = row[:first_name]
-    zipcode = row[:zipcode]
-    puts "#{name} #{zipcode}"
-    puts name
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name} #{zipcode}"
 end
